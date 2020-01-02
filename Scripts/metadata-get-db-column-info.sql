48d0fc1d476f02a7bc5cd9bfe52300e68f05e417
Steven Benner <benner.steven@gmail.com>
2020-01-02 08:42:44 -0600

    added default constraint info to columns metadata query

--- Scripts/metadata-get-db-column-info.sql
+++ Scripts/metadata-get-db-column-info.sql
@@ -1,10 +1,10 @@
-
 -- get database column metadata
     -- To Add: Computed Column Definition
 SELECT objects.type_desc AS obj_type
      , schemas.name AS schema_name
      , objects.name AS obj_name
      , columns.name AS col_name
+     , columns.is_identity
      , types.name AS data_type
      , concat(types.name ,
                     CASE
@@ -19,15 +19,17 @@ SELECT objects.type_desc AS obj_type
 								END , ')')
                         ELSE ''
                     END) AS col_definition
+	 , case when columns.is_nullable = 'true' then 'null' else 'not null' end as nullable
+	 , default_constraints.definition as default_constraint_definition
      , columns.max_length
      , columns.precision
      , columns.scale
-     , columns.collation_name
-     , columns.is_nullable
-     , columns.is_identity
      , columns.is_computed
+     , columns.collation_name
 FROM   sys.objects
        INNER JOIN sys.columns ON objects.object_id = columns.object_id
 INNER JOIN sys.schemas ON objects.schema_id = schemas.schema_id
 INNER JOIN sys.types ON columns.user_type_id = types.user_type_id
+left outer join sys.default_constraints on columns.object_id = default_constraints.parent_object_id
+	and columns.column_id = default_constraints.parent_column_id
 WHERE  schemas.schema_id <> 4 -- ignore sys schema

155cb25cbdca8d48dd2f2ad729663efd3fa27fba
Steven Benner <benner.steven@gmail.com>
2020-01-13 07:05:42 -0600

    added fields for data driven subscription and other metadata

--- Scripts/ssrs-get-all-subscriptions.sql
+++ Scripts/ssrs-get-all-subscriptions.sql
@@ -4,15 +4,22 @@ USE [ReportServer];
 SELECT LEFT([Catalog].[Path] , LEN([Catalog].[Path]) - CHARINDEX('/' , REVERSE([Catalog].[Path]))) AS Folder
      , [Catalog].Name AS ReportName
      , Subscriptions.[Description]
+     , CASE
+           WHEN Subscriptions.DataSettings IS NOT NULL THEN 'Data Driven'
+           ELSE 'Normal'
+       END AS SubscriptionType
      , Subscriptions.EventType
      , Subscriptions.DeliveryExtension
-     , Subscriptions.LastStatus
+     , Subscriptions.LastStatus AS LastRunStatus
      , Subscriptions.LastRunTime
-     , Schedule.Name AS ScheduleName
      , Schedule.ScheduleID AS AgentJobName
      , [Catalog].[Description] AS ReportDescription
      , Users.UserName AS SubscriptionOwner
      , Subscriptions.ModifiedDate
+     , Schedule.Name AS ScheduleName
+     , CAST(Subscriptions.ExtensionSettings AS XML) AS ExtensionSettings
+     , CAST(Subscriptions.MatchData AS XML) AS MatchData
+     , CAST(Subscriptions.DataSettings AS XML) AS DataSettings
 FROM   dbo.Subscriptions
        INNER JOIN dbo.Users ON Subscriptions.OwnerID = Users.UserID
 INNER JOIN dbo.[Catalog] ON Subscriptions.Report_OID = [Catalog].ItemID

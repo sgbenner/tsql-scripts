23b91619becbab7a4636507007dc21ad90903e2f
Steven Benner <benner.steven@gmail.com>
2020-01-20 08:01:17 -0600

    updated to use event_messages table which includes package task that failed

--- Scripts/ssisdb-helpful-catalog-queries.sql
+++ Scripts/ssisdb-helpful-catalog-queries.sql
@@ -275,43 +275,19 @@ SELECT executions.execution_id
      , executions.folder_name
      , executions.project_name
      , executions.package_name
+     , event_messages.execution_path
+     , event_messages.event_name
+     , CAST(event_messages.message_time AS DATETIME2(3)) AS message_time
+     , event_messages.message
      , executions.environment_folder_name AS env_folder
      , executions.environment_name AS env_name
      , executions.executed_as_name
-     , executions.server_name
-     , CASE operation_messages.message_source_type
-           WHEN 10 THEN 'Entry APIs, such as T-SQL and CLR Stored procedures'
-           WHEN 20 THEN 'External process used to run package (ISServerExec.exe)'
-           WHEN 30 THEN 'Package-level objects'
-           WHEN 40 THEN 'Control Flow tasks'
-           WHEN 50 THEN 'Control Flow containers'
-           WHEN 60 THEN 'Data Flow task'
-           ELSE ''
-       END AS message_source
-     , CASE operation_messages.message_type
-           WHEN-1 THEN 'Unknown'
-           WHEN 120 THEN 'Error'
-           WHEN 110 THEN 'Warning'
-           WHEN 70 THEN 'Information'
-           WHEN 10 THEN 'Pre-validate'
-           WHEN 20 THEN 'Post-validate'
-           WHEN 30 THEN 'Pre-execute'
-           WHEN 40 THEN 'Post-execute'
-           WHEN 60 THEN 'Progress'
-           WHEN 50 THEN 'StatusChange'
-           WHEN 100 THEN 'QueryCancel'
-           WHEN 130 THEN 'TaskFailed'
-           WHEN 90 THEN 'Diagnostic'
-           WHEN 200 THEN 'Custom'
-           WHEN 140 THEN 'DiagnosticEx'
-           ELSE ''
-       END AS message_type
-     , cast(operation_messages.message_time as datetime2(3)) as message_time
-     , operation_messages.message
+     , event_messages.message_source_name
+-- select *
 FROM   catalog.executions
 INNER JOIN catalog.operations ON execution_id = operation_id
-INNER JOIN catalog.operation_messages ON operations.operation_id = operation_messages.operation_id
-WHERE  operation_messages.message_type IN(120 , 130) -- Error, TaskFailed
-	and message_time >= getdate() - 30 --- just showing last 30 days to keep result set a little smaller
+INNER JOIN catalog.event_messages ON operations.operation_id = event_messages.operation_id
+WHERE  event_messages.message_type IN(120 , 130) -- Error, TaskFailed
+       AND event_messages.message_time >= GETDATE() - 30 --- just showing last 30 days to keep result set a little smaller
 ORDER BY execution_id DESC
        , message_time;

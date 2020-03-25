504274c81da5ffbdc80f8604a330eafb316f88dd
Steven Benner <benner.steven@gmail.com>
2020-03-25 17:00:33 -0400

    added time until complete and restore head only commands

--- Scripts/dba-backup-restore-progress.sql
+++ Scripts/dba-backup-restore-progress.sql
@@ -5,6 +5,8 @@ SELECT session_id AS SPID
      , start_time
      , percent_complete
      , DATEADD(second , estimated_completion_time / 1000 , GETDATE()) AS estimated_completion_time
+     , CAST(DATEDIFF(second , GETDATE() , DATEADD(second , estimated_completion_time / 1000 , GETDATE())) / 60.0 AS decimal(10 , 2)) AS time_until_complete_min
+     , DATEDIFF(second , GETDATE() , DATEADD(second , estimated_completion_time / 1000 , GETDATE())) AS time_until_complete_sec
 FROM   sys.dm_exec_requests r
        CROSS APPLY sys.dm_exec_sql_text(r.sql_handle) a
-WHERE  r.command IN('BACKUP DATABASE' , 'RESTORE DATABASE');
+WHERE  r.command IN('BACKUP DATABASE' , 'RESTORE DATABASE' , 'RESTORE HEADERONLY');

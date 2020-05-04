5382ad938ec7465d46cc1cda29aa9f56f3bc3a6c
Steven Benner <benner.steven@gmail.com>
2020-05-04 16:29:43 -0400

    adding created by and updated by

--- Scripts/ssrs-report-execution-metrics.sql
+++ Scripts/ssrs-report-execution-metrics.sql
@@ -1,12 +1,14 @@
 select [Catalog].[Path] as ReportPath
 	, [Catalog].[Name] as ReportName
 	, [Catalog].Hidden
-	, [Catalog].CreationDate
-	, [Catalog].ModifiedDate
-	, [Catalog].CreatedByID
-	, [Catalog].ModifiedByID
+	, cast([Catalog].CreationDate as date) as CreationDate
+	, cast([Catalog].ModifiedDate as date) as ModifiedDate
+	, [CreatedBy].UserName as CreatedBy
+	, ModifiedBy.UserName as ModifiedBy
 	, ExecutionMetrics.*
 from [ReportServer].[dbo].[Catalog]
+left outer join [ReportServer].[dbo].[Users] as CreatedBy on [Catalog].CreatedByID = CreatedBy.UserID
+left outer join [ReportServer].[dbo].[Users] as ModifiedBy on [Catalog].ModifiedByID = ModifiedBy.UserID
 left outer join (
 		select ReportID, count(*) as ExecutionCount, sum(case when Status = 'rsSuccess' then 1 else 0 end) as TotalSuccess
 			, sum(case when Status <> 'rsSuccess' then 1 else 0 end) as TotalNonSuccess

49222157b6e1afdd847428e658c4e332e31a95e5
Steven Benner <benner.steven@gmail.com>
2020-10-23 09:16:46 -0500

    sql throws error if internal avg calculation exceeds int data type. Fixed issue and reformatted

--- Scripts/ssrs-report-execution-metrics.sql
+++ Scripts/ssrs-report-execution-metrics.sql
@@ -1,27 +1,42 @@
-select [Catalog].[Path] as ReportPath
-	, [Catalog].[Name] as ReportName
-	, [Catalog].Hidden
-	, cast([Catalog].CreationDate as date) as CreationDate
-	, cast([Catalog].ModifiedDate as date) as ModifiedDate
-	, [CreatedBy].UserName as CreatedBy
-	, ModifiedBy.UserName as ModifiedBy
-	, ExecutionMetrics.*
-from [ReportServer].[dbo].[Catalog]
-left outer join [ReportServer].[dbo].[Users] as CreatedBy on [Catalog].CreatedByID = CreatedBy.UserID
-left outer join [ReportServer].[dbo].[Users] as ModifiedBy on [Catalog].ModifiedByID = ModifiedBy.UserID
-left outer join (
-		select ReportID, count(*) as ExecutionCount, sum(case when Status = 'rsSuccess' then 1 else 0 end) as TotalSuccess
-			, sum(case when Status <> 'rsSuccess' then 1 else 0 end) as TotalNonSuccess
-			, max(case when Status = 'rsSuccess' then TimeStart else null end) as LastSuccessfulRunTime
-			, max(TimeStart) as LastRunTime
-			, avg(TimeDataRetrieval) as AvgDataRetrievalTime
-			, avg(TimeProcessing) as AvgTimeProcessing
-			, avg(TimeRendering) as AvgTimeRendering
-			, avg([RowCount]) as AvgRowCount
-			, min([RowCount]) as MinRowCount
-			, max([RowCount]) as MaxRowCount
-		from [ReportServer].[dbo].[ExecutionLog]
-		group by ReportID
-		) as ExecutionMetrics on [Catalog].ItemID = ExecutionMetrics.ReportID
-where [Catalog].[Type] = 2 -- report type
-order by 1,2
+USE [ReportServer];
+
+SELECT [Catalog].[Path] AS ReportPath
+     , [Catalog].[Name] AS ReportName
+     , [Catalog].Hidden
+     , CAST([Catalog].CreationDate AS DATE) AS CreationDate
+     , CAST([Catalog].ModifiedDate AS DATE) AS ModifiedDate
+     , [CreatedBy].UserName AS CreatedBy
+     , ModifiedBy.UserName AS ModifiedBy
+     , ExecutionMetrics.*
+FROM [dbo].[Catalog]
+LEFT OUTER JOIN [dbo].[Users] AS CreatedBy ON [Catalog].CreatedByID = CreatedBy.UserID
+LEFT OUTER JOIN [dbo].[Users] AS ModifiedBy ON [Catalog].ModifiedByID = ModifiedBy.UserID
+LEFT OUTER JOIN
+     (
+         SELECT ReportID
+              , COUNT(*) AS ExecutionCount
+              , SUM(CASE
+                        WHEN STATUS = 'rsSuccess' THEN 1
+                        ELSE 0
+                    END) AS TotalSuccess
+              , SUM(CASE
+                        WHEN STATUS <> 'rsSuccess' THEN 1
+                        ELSE 0
+                    END) AS TotalNonSuccess
+              , MAX(CASE
+                        WHEN STATUS = 'rsSuccess' THEN TimeStart
+                        ELSE NULL
+                    END) AS LastSuccessfulRunTime
+              , MAX(TimeStart) AS LastRunTime
+              , AVG(CAST(TimeDataRetrieval AS BIGINT)) AS AvgDataRetrievalTime
+              , AVG(CAST(TimeProcessing AS BIGINT)) AS AvgTimeProcessing
+              , AVG(CAST(TimeRendering AS BIGINT)) AS AvgTimeRendering
+              , AVG([RowCount]) AS AvgRowCount
+              , MIN([RowCount]) AS MinRowCount
+              , MAX([RowCount]) AS MaxRowCount
+         FROM [dbo].[ExecutionLog]
+         GROUP BY ReportID
+     ) AS ExecutionMetrics ON [Catalog].ItemID = ExecutionMetrics.ReportID
+WHERE [Catalog].[Type] = 2 -- report type
+ORDER BY 1
+       , 2;
